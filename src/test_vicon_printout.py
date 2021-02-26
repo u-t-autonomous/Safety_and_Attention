b2671e397cd7dc1f0596978e4f7e2497a70ddb8e
jesse quattrociocchi <jesse.quattrociocchi@gmail.com>
2021-02-25 22:29:30 -0600

    Adding example for accessing vicon via python for state information of all 3 TBs

---
+++ src/test_vicon_printout.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env/ python2
+
+from geometry_msgs.msg import TransformStamped
+
+# import sys
+# import rospy
+import numpy as np
+# import time
+
+
+class Tracker(object):
+    def __init__(self):
+        # self.pub = rospy.Publisher('/ready_start_cmd', Bool, queue_size=1)
+        self.data = [None, None, None, None]
+
+        sub1 = rospy.Subscriber('/vicon/TB1/TB1', TransformStamped, self.viconCB1)
+        sub2 = rospy.Subscriber('/vicon/TB2/TB2', TransformStamped, self.viconCB2)
+        sub3 = rospy.Subscriber('/vicon/TB3/TB3', TransformStamped, self.viconCB3)
+
+    def viconCB1(self, msg):
+        self.data[1] = msg.transform
+
+    def viconCB2(self, msg):
+        self.data[2] = msg.transform
+
+    def viconCB3(self, msg):
+        self.data[3] = msg.transform
+
+def make_user_wait(msg="Enter exit to exit"):
+    data = raw_input(msg + "\n")
+    if data == 'exit':
+        sys.exit()
+
+def wait_for_time():
+    """Wait for simulated time to begin """
+    while rospy.Time().now().to_sec() == 0:
+        pass
+
+if __name__ == "__main__":
+    rospy.init_node('test_vicon', anonymous=True)
+    wait_for_time()
+    tr = Tracker()
+    rospy.sleep(2)
+    # rate = rospy.Rate(0.5)
+    while not rospy.is_shutdown():
+        print('TB1 translation is:\n{}'.format(tr.data[1].rotation))
+        print('TB2 translation is:\n{}'.format(tr.data[2].rotation))
+        print('TB3 translation is:\n{}'.format(tr.data[3].rotation))
+        make_user_wait()
+        # rate.sleep()
+
+
+
+
